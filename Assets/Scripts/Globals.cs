1085df5487c0fa43f0da9db93288e45102807338
MONO DEV <monodev619@gmail.com>
2017-05-30 10:26:39 +0800

    add global script
    In here exist global variables.

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public static class Globals
+{
+    public static int[] Levels = { 1000, 2000, 5000, 7000 };
+}
